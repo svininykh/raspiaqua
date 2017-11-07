9f5ff77053a0956883907bf72ebcde18af18b73a
Andrey Svininykh <svininykh@gmail.com>
2017-11-07 21:53:21 +0600

    Add function for determination sunrise and sunset for current day.

---
+++ src/main/java/io/hackaday/raspiaqua/service/DetermineSunriseSunset.java
@@ -0,0 +1,95 @@
+package io.hackaday.raspiaqua.service;
+
+import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
+import com.luckycatlabs.sunrisesunset.dto.Location;
+import java.io.IOException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author svininykh-av
+ */
+public class DetermineSunriseSunset {
+
+    Properties prop = new Properties();
+    Calendar today = Calendar.getInstance();
+    Calendar sunrise = Calendar.getInstance();
+    Calendar sunset = Calendar.getInstance();
+    SunriseSunsetCalculator calculator;
+
+    public DetermineSunriseSunset(Date date) throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        prop.load(loader.getResourceAsStream("config.properties"));
+        today = Calendar.getInstance(
+                TimeZone.getTimeZone(prop.getProperty("place.timezone", "Europe/London")),
+                Locale.getDefault()
+        );
+        calculator = new SunriseSunsetCalculator(
+                new Location(prop.getProperty("place.latitude", "0.0"), prop.getProperty("place.longitude", "0.0")),
+                today.getTimeZone()
+        );
+        today.setTime(date);
+        sunrise = calculator.getOfficialSunriseCalendarForDate(today);
+        sunset = calculator.getOfficialSunsetCalendarForDate(today);
+    }
+
+    private Calendar getNextSunrise() {
+        Calendar sunriseDay = today;
+        if (sunriseDay.after(sunrise)) {
+            sunriseDay.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return calculator.getAstronomicalSunriseCalendarForDate(sunriseDay);
+    }
+
+    private Calendar getNextSunset() {
+        Calendar sunsetDay = today;
+        if (sunsetDay.after(sunset)) {
+            sunsetDay.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return calculator.getAstronomicalSunriseCalendarForDate(sunsetDay);
+    }
+
+    private boolean isDayNow() {
+        return today.after(sunrise) && today.before(sunset);
+    }
+
+    public long getDayDurationMinutes() {
+        if (!isDayNow()) {
+            return 0;
+        }
+        return today.toInstant().until(sunset.toInstant(), ChronoUnit.MINUTES);
+    }
+
+    public long getNightDurationMinutes() {
+        if (isDayNow()) {
+            return 0;
+        }
+        if (today.after(sunset)) {
+            return today.toInstant().until(getNextSunrise().toInstant(), ChronoUnit.MINUTES);
+        }
+        return today.toInstant().until(sunrise.toInstant(), ChronoUnit.MINUTES);
+    }
+
+    public Calendar getSunrise() {
+        return sunrise;
+    }
+
+    public Calendar getSunset() {
+        return sunset;
+    }
+
+    public Calendar getToday() {
+        return today;
+    }
+    
+    
+
+}
--- src/main/java/io/hackaday/raspiaqua/service/ServerHandler.java
+++ src/main/java/io/hackaday/raspiaqua/service/ServerHandler.java
@@ -3,6 +3,7 @@ package io.hackaday.raspiaqua.service;
 import io.hackaday.raspiaqua.proto.Aquarium;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import java.util.Date;
 
 /**
  *
@@ -13,12 +14,13 @@ public class ServerHandler extends SimpleChannelInboundHandler<Aquarium.AquaRequ
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Aquarium.AquaRequest msg)
             throws Exception {
+        DetermineSunriseSunset dss = new DetermineSunriseSunset(new Date());
         Aquarium.AquaResponse.Builder builder = Aquarium.AquaResponse.newBuilder();
         if (msg.getEquipmentType() == Aquarium.Equipment.LIGHTING) {
             builder.setLightingLamp(Aquarium.Lighting.newBuilder()
                     .setBasic(Aquarium.Condition.newBuilder()
-                            .setStatus(Aquarium.Condition.Status.ON)
-                            .setDuration(3)
+                            .setStatus(dss.getNightDurationMinutes() > 0 ? Aquarium.Condition.Status.ON : Aquarium.Condition.Status.OFF)
+                            .setDuration(dss.getNightDurationMinutes() > 0 ? (int) dss.getNightDurationMinutes() : (int) dss.getDayDurationMinutes())
                             .build()
                     )
             );
